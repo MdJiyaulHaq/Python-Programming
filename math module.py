4dd9179fff6b5298b7e2205337260263d0329f87
Zia-ul-haq <muhammadziaulhaq78692@gmail.com>
2021-10-22 21:30:47 +0545

    math in python

---
+++ math module.py
@@ -0,0 +1,30 @@
+# there are many built-in math functions in python
+x = min(4, 6, 3, 8, 8, 9)
+y = max(4, 6, 3, 8, 8, 9)
+print(x)
+print(y)
+a = pow(3, 4)
+print(a)
+
+import math
+
+x = math.sqrt(81)
+y = math.ceil(78.6)
+z = math.floor(78.6)
+print(x, y, z)
+
+# there are many constants in math that python support
+e = math.e
+print(e)
+
+pi = math.pi
+print(pi)
+
+tau = math.tau
+print(tau)
+
+inf = math.inf
+print(inf)
+
+nan = math.nan
+print(nan)
