1f2b8bfba5e5b4eb5d96bf8a3f91415a21c4caa3
Zia-ul-haq <muhammadziaulhaq78692@gmail.com>
2021-10-23 21:20:28 +0545

    format string updated

--- Python Try Except/use of [finally] keyword.py
+++ Python Try Except/use of [finally] keyword.py
@@ -1,14 +1,14 @@
 # finally: keyword can be as useful as oxygen sometimes
 
 try:
-    f = open("README.md")
+    f = open("text.txt")
     try:
         f.write("Thank you.")
     except:
         print("Couldn't perform write operation")
+    else:
+        print("Written to the file successfully")
     finally:
         f.close()  # if anything goes wrong, we can still rul finally and close file.
 except:
     print("Couldn't open file")
-else:
-    print("Written to the file successfully")
