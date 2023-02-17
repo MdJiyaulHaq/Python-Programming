a6a948bcd0085198fcb86c6a46405fb45b4ebac8
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2023-02-17 20:38:47 +0545

    updated

--- File Handling/create and delete files.py
+++ File Handling/create and delete files.py
@@ -1,3 +1,4 @@
+
 f = open("newFile1", "x")  # - Create - will create a file, returns an error if the file exist
 f.close()
 f = open("newFile2", "a")  # - Append - will create a file if the specified file does not exist
@@ -10,12 +11,12 @@ import os
 
 os.remove("newFile1")  # removes specified file
 os.rmdir("an empty folder")  # removes specified folder
-# note: removes only if the folder is empty, and throws error if folder doesn't esist
+# note: removes only if the folder is empty, and throws error if folder doesn't exist
 
 
-# to avoid an error, check the file if it exist before attempting to delete it.
+# to avoid an error, check the file if it exists before attempting to delete it.
 if os.path.exists("newFile2"):
-    print("yes the File exists, let me delete it.")
+    print("yes the File exists, i am deleting it.")
     os.remove("newFile2")
     print("done! the File was deleted successfully")
 else:
@@ -23,7 +24,7 @@ else:
 
 # similarly
 if os.path.exists("newFile3"):
-    print("yes the File exists, let me delete it.")
+    print("yes the File exists, i am deleting it it.")
     os.remove("newFile3")
     print("done! the File was deleted successfully")
 else:
